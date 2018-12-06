3cff15b4c1e1674c038c8d1c3a6d2c7a68035afb
oktbabs <35835776+oktbabs@users.noreply.github.com>
2018-12-06 14:44:11 +0000

    Update InstallDatabaseServer.sh

--- InstallDatabaseServer.sh
+++ InstallDatabaseServer.sh
@@ -1,18 +1,18 @@
-create database oelvmdb controlfile reuse Logfile 
-Group 1 ('/mydatabases/oelvmdb/log01a.log', 
-'/mydatabase02/oelvmdb/log01b.log') size 50M,
-Group 2 ('/mydatabases/oelvmdb/log02a.log',
-'/mydatabase02/oelvmdb/log02b.log') size 50M,
-Group 3 ('/mydatabases/oelvmdb/log03a.log',
-'/mydatabase02/oelvmdb/log03b.log') size 50M,
-Group 4 ('/mydatabases/oelvmdb/log04a.log',
-'/mydatabase02/oelvmdb/log04b.log') size 50M
-Datafile '/mydatabases/oelvmdb/oelvmdb_system.dbf' 250M 
+create database vanilladb controlfile reuse Logfile 
+Group 1 ('/mydatabases/vanilladb/log01a.log', 
+'/mydatabase02/vanilladb/log01b.log') size 50M,
+Group 2 ('/mydatabases/vanilladb/log02a.log',
+'/mydatabase02/vanilladb/log02b.log') size 50M,
+Group 3 ('/mydatabases/vanilladb/log03a.log',
+'/mydatabase02/vanilladb/log03b.log') size 50M,
+Group 4 ('/mydatabases/vanilladb/log04a.log',
+'/mydatabase02/vanilladb/log04b.log') size 50M
+Datafile '/mydatabases/vanilladb/vanilladb_system.dbf' 250M 
 AUTOEXTEND ON MAXSIZE 500M
-Undo tablespace oelvmdb_undo1
-Datafile '/mydatabases/oelvmdb/oelvmdb_undo1.dbf' 50M
-Default temporary tablespace oelvmdbtemp tempfile
-'/mydatabases/oelvmdb/oelvmdbtemp1.dbf' size 75M
+Undo tablespace vanilladb_undo1
+Datafile '/mydatabases/vanilladb/vanilladb_undo1.dbf' 50M
+Default temporary tablespace vanilladbtemp tempfile
+'/mydatabases/vanilladb/vanilladbtemp1.dbf' size 75M
 Extent management local 
 Character set AL32UTF8
 MAXLOGFILES 10
